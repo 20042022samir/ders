4fa5ad4ab014fed5515c6e1fc76eba5c75b1bcb3
20042022samir <samirfi@code.edu.az>
2023-03-06 13:28:05 -0800

    homework

---
+++ kings.cs
@@ -0,0 +1,40 @@
+﻿
+using System.Globalization;
+using System.Text.RegularExpressions;
+
+class CheckPasword
+{
+    CheckPasword(string username, string pasword)
+    {
+        public string pasword = ("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
+    public string username;
+    public string Username
+    {
+        get => username;
+        set
+        {
+            if (username != value)
+            {
+                Console.WriteLine("olmaz");
+            }
+        }
+    }
+
+
+
+    public string Pasword
+    {
+        get => pasword;
+        set
+        {
+
+            if (value.Length > 8)
+            {
+                Console.WriteLine("uygundur");
+            }
+        }
+    }
+}
+
+   
+
---
+++ program.cs
@@ -0,0 +1,12 @@
+﻿
+
+using System.Text.RegularExpressions;
+
+CheckPasword operation = CheckPasword();
+Console.WriteLine("salam zehmet olmasa sifreni daxil edin");
+operation.Pasword = Console.ReadLine();
+
+Console.WriteLine("zehmet olmasa istifadeci adinizi daxil edin");
+operation.username = Console.ReadLine();
+
+new Regex("^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9]).{8,}$");
